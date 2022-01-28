8014e9a4234bc33548878e264d6d94560acafba2
iamindika <ikolamba2@gmail.com>
2022-01-27 21:34:00 -0500

    define qr code image component

---
+++ src/components/QRCode.js
@@ -0,0 +1,14 @@
+const qRCode = require("../assets/images/image-qr-code.png");
+
+export default function QRCode() {
+  return (
+    <header className="hero">
+      <div className="hero__img">
+        <img
+          src={qRCode}
+          alt="qr code"
+        />s
+      </div>
+    </header>
+  )
+}
