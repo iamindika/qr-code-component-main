3ecb600db35a11080570a3dde24c8a4c6f3de03e
iamindika <ikolamba2@gmail.com>
2022-01-27 21:27:15 -0500

    define qr description component

---
+++ src/components/QRDesc.js
@@ -0,0 +1,8 @@
+export default function QRDesc() {
+  return (
+    <div className="qr-desc">
+      <h1 className="qr-desc__title">Improve your front-end skills by building projects</h1>
+      <p className="qr-desc__text">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level.s</p>
+    </div>
+  )
+}
